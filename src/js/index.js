3908067035e4bde8e70969d6872233709b67efc0
Christiano0407 <placebo0704@gmail.com>
2021-05-21 09:37:23 -0500

    practice02

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,37 @@ console.log(typeof(myNumber));
 
 let myString = `Hello World`; 
 console.log(typeof(myString)); 
+
+/* Incremento */
+let x = 1; 
+let y = ++x;
+
+console.log(x); 
+console.log(y); 
+
+/* Decremento */
+let z = x++; 
+
+console.log(z); 
+
+          /* Boolean /  Valor y el tipo */ 
+
+ /* !true == false */
+ /* !false == true */
+/*  !!true == true */
+
+/* true === true =>  true*/
+/* true === false =>  false*/
+/* true !== false =>  true*/
+/* false !== false  =>  false*/
+
+/* 5 > 3 ==> true */
+/* 5 < 3 ==> false */
+/* 3 >= 3 ==> true */
+/* 2 <= 1 ==> false */
+/* "e" < "d" ==> false */
+
+// Operador ==> AND (&&)
+// Operador ==> OR (||)
+// Operador ==> !==
+
