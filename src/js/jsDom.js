4c94c0689da91bc4f6c368ea6a7aff9f3fd649a9
Christiano0407 <placebo0704@gmail.com>
2021-06-07 09:13:07 -0500

    Documents

--- src/js/jsDom.js
+++ src/js/jsDom.js
@@ -5,3 +5,19 @@ console.log("Hello World");
 
 /* Object Documents */
 /* Nos permite realizar acciones sobre nuestra página web. */
+
+let saludo = "Hola amigo y familia"; 
+console.log(saludo); 
+
+
+   /*  Obtener Elementos a través de "ID" */
+const title = document.getElementById(`title`); 
+console.log(title); 
+title.innerHTML = "Cursos con Javascript"; 
+
+const description = document.getElementById(`description`); 
+console.log(description); 
+description.innerHTML = "Listado de Cursos para Programación : "; 
+
+
+  /*     Obtener Elementos a través de "Class" */
