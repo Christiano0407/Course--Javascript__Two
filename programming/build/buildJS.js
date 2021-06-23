311da35b7c6f0454b122eaa436f66d9843f0da04
Christiano0407 <placebo0704@gmail.com>
2021-06-23 11:52:59 -0500

    btn

--- programming/build/buildJS.js
+++ programming/build/buildJS.js
@@ -2,6 +2,7 @@
 
 // Agregar texto al DOM = HTML = Javascript: 
 
+// Exercise Text =====
 let welcomeEl = document.getElementById(`welcome-el`); 
 
 let names = "Jennifer Amanda"; 
@@ -12,3 +13,29 @@ welcomeEl.style.color = "red";
 welcomeEl.style.fontSize =  "24px"; 
 
 welcomeEl.innerText += " From New York"; 
+
+//Exercise Count ====
+// Variables ===HTML/DOMG/id: 
+const btn = document.getElementById(`btn`); 
+const count= document.getElementById(`count-el`);
+const saveBtn = document.getElementById(`btn-save`); 
+const saveEl = document.getElementById(`save-el`); 
+
+// Contador: 
+let countOne = 0; 
+
+// Evento Contador:
+btn.addEventListener(`click`, () => {
+    countOne = countOne + 1; 
+    
+    count.innerText = countOne; 
+})
+
+//Evento Guardar/save: 
+saveBtn.addEventListener(`click`, () => {
+   save = () => {
+       console.log(countOne); 
+   }
+   save(); 
+})
+console.log(countOne); 
