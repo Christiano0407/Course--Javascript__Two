9a3d8f34c30493571f169ab77b26066fa44bc3dd
Christiano0407 <placebo0704@gmail.com>
2021-07-14 12:26:40 -0500

    challenge-calculator

---
+++ challengeTwo/appCalculator.js
@@ -0,0 +1,12 @@
+console.group("Calculator"); 
+const numOne = document.getElementById(`num1-el`); 
+const numTwo = document.getElementById(`num2-el`); 
+const btnOne = document.getElementById(`btn1`); 
+const btnTwo = document.getElementById(`btn2`); 
+const btnThree = document.getElementById(`btn3`); 
+const btnFour = document.getElementById(`btn4`); 
+const suma = document.getElementById(`sum-el`); 
+
+
+
+console.groupEnd(); 
