fe8427c197f0a514b466283b04c9109c5ac015bd
Christiano0407 <placebo0704@gmail.com>
2021-07-19 15:54:28 -0500

    random01

--- sectionThree/mathJs.js
+++ sectionThree/mathJs.js
@@ -1,7 +1,11 @@
 console.group("MathRandom"); 
 //< ================== MathRandom ==================== >
 
+// >>>>>> Random === aleatorio <<<<<<<
+let randomNumber = Math.random(); 
+console.log(randomNumber); 
 
+// >> Random Game <<
 
 
 console.groupEnd(); 
