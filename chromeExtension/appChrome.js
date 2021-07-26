511e752aaae49fdac0654db542a00548b6a64806
Christiano0407 <placebo0704@gmail.com>
2021-07-26 09:36:27 -0500

    chorme-Exercise

--- chromeExtension/appChrome.js
+++ chromeExtension/appChrome.js
@@ -1,7 +1,24 @@
 console.group("Build Chrome"); 
+// =========== =========== <<<<< Chrome Exercise >>>>>> ================= ================
+// =========== <<<<< Variables >>>>>> ================= 
+const input = document.getElementById(`input-el`); 
+const button = document.getElementById(`input-btn`); 
 
+let myLeads = ["www.awesomeLead.com", "www.epiclead.com", "www.greatCloud.com"]; 
 
+// >>>>>>>>>>>> ====== Eventos ===== <<<<<<<<<<<<<
+button.addEventListener(`click`, () => {
+   console.log("I´m Button!"); 
+// >>>> Methods >>>>>
+   myLeads.push(input.value); 
+   console.log(myLeads); 
 
+   
+});
+
+for(let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]); 
+}
 
 
 
